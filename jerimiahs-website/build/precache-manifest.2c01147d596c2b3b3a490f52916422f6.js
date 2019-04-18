d83b9a282ff6379ee73d0750b07d7fe460d33eec
Jerimiah Huston <jhuston357@live.com>
2019-04-18 19:15:18 -0400

    fixed gitignore so i can push production build

---
+++ jerimiahs-website/build/precache-manifest.2c01147d596c2b3b3a490f52916422f6.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c635abc119973374c3ca7b9d1bcf908",
+    "url": "/static/media/4.0c635abc.jpg"
+  },
+  {
+    "revision": "2f7ffd5ce494cdba3733",
+    "url": "/static/css/main.594ca015.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9e36a5fb0678cd7b4e132e8174ad4931",
+    "url": "/static/media/waterfall.9e36a5fb.jpg"
+  },
+  {
+    "revision": "b0caeed0ea37058a2d9d",
+    "url": "/static/js/2.32d902cb.chunk.js"
+  },
+  {
+    "revision": "63d89f4bb0b61e4f8d6d4d8e830d90bf",
+    "url": "/static/media/jjhCF.63d89f4b.jpg"
+  },
+  {
+    "revision": "3a9dd41fca46866259ae345a34562d1a",
+    "url": "/static/media/hockey.3a9dd41f.jpg"
+  },
+  {
+    "revision": "e6784741fc51379f5ebde09139c64658",
+    "url": "/static/media/snowboarding.e6784741.png"
+  },
+  {
+    "revision": "1fc8e17ba8584223409ef41eb4c47575",
+    "url": "/static/media/wtbrdalg169.1fc8e17b.jpg"
+  },
+  {
+    "revision": "2c401f0ff6ab67789dd1ca5f57b4056c",
+    "url": "/static/media/chuckapplistscreen.2c401f0f.png"
+  },
+  {
+    "revision": "71b952232787c3a066c7e2b4527939de",
+    "url": "/static/media/chuckappeditscreen.71b95223.png"
+  },
+  {
+    "revision": "c5d41b6f72e44ed9b1b365b0ef02b764",
+    "url": "/static/media/1.c5d41b6f.jpg"
+  },
+  {
+    "revision": "5d85a601c1d0a1b1f89e3572fb468146",
+    "url": "/static/media/2.5d85a601.jpg"
+  },
+  {
+    "revision": "0f4963875155d83fcafdd371abd68aa3",
+    "url": "/static/media/3.0f496387.jpg"
+  },
+  {
+    "revision": "2f7ffd5ce494cdba3733",
+    "url": "/static/js/main.5182c873.chunk.js"
+  },
+  {
+    "revision": "5c7d59e79f2109f8538118e730f97eea",
+    "url": "/static/media/5.5c7d59e7.jpg"
+  },
+  {
+    "revision": "4b61d57ed6df7508f386c9ad6084e3e8",
+    "url": "/static/media/6.4b61d57e.jpg"
+  },
+  {
+    "revision": "d02794cf0f8632f3bbb69907b803a3c0",
+    "url": "/static/media/7.d02794cf.jpg"
+  },
+  {
+    "revision": "8cf7c365d1b646b3dce40bf93e380bd4",
+    "url": "/static/media/8.8cf7c365.jpg"
+  },
+  {
+    "revision": "acb0a04a2a5ea0ed58a598bd87dd3561",
+    "url": "/static/media/9.acb0a04a.jpg"
+  },
+  {
+    "revision": "0b26fb6c3ee5565a8b77ff31b9c53611",
+    "url": "/static/media/10.0b26fb6c.jpg"
+  },
+  {
+    "revision": "3e3f071aee013ffa33b31095371faa94",
+    "url": "/static/media/11.3e3f071a.jpg"
+  },
+  {
+    "revision": "5b7591cb1b7e5c6e91d07189339f08ce",
+    "url": "/static/media/12.5b7591cb.jpg"
+  },
+  {
+    "revision": "c19b391c654238d484e45c310b6381ca",
+    "url": "/static/media/13.c19b391c.jpg"
+  },
+  {
+    "revision": "bd0864f624757912440fc6107b696e3a",
+    "url": "/static/media/14.bd0864f6.jpg"
+  },
+  {
+    "revision": "862559f2d55945ef688f7088cdd97fa0",
+    "url": "/static/media/jtownview2long.862559f2.jpg"
+  },
+  {
+    "revision": "b0caeed0ea37058a2d9d",
+    "url": "/static/css/2.dec465b2.chunk.css"
+  },
+  {
+    "revision": "9f5b2c0a856bce1b042a958c36cb68a3",
+    "url": "/index.html"
+  }
+];

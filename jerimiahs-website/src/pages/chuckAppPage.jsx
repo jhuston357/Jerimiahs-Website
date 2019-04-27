06cfb679ae447177ee4842d826b90f6c08ea1786
Jerimiah Huston <jhuston357@live.com>
2019-04-26 21:19:49 -0400

    changed api to https

--- jerimiahs-website/src/pages/chuckAppPage.jsx
+++ jerimiahs-website/src/pages/chuckAppPage.jsx
@@ -1,62 +1,12 @@
 import React, { Component } from "react";
 
 class ChuckAppPage extends Component {
-<<<<<<< HEAD
-  state = {};
-
-  componentDidMount() {
-    function jsontotable(json) {
-      var output = "";
-      var ckarr = JSON.parse(json);
-      var x;
-      output =
-        "<table style='width:100%'><tr><th>ID</th><th>ChuckQuote</th><th>EnteredBy</th><th>QuoteDate</th></tr>";
-      for (x in ckarr) {
-        output =
-          output +
-          "<tr><td>" +
-          ckarr[x].ID +
-          "</td><td>" +
-          ckarr[x].ChuckQuote +
-          "</td><td>" +
-          ckarr[x].EnteredBy +
-          "</td><td>" +
-          ckarr[x].QuoteDate +
-          "</td></tr>";
-      }
-      return output + "</table>";
-    }
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("chucktable").innerHTML = jsontotable(
-          this.responseText
-        );
-      }
-    };
-    xhttp.open("GET", "/api/chuckdata.php", true);
-    xhttp.send();
-  }
-
-  render() {
-    return (
-      <div class="padding">
-        <div class="container">
-          <div class="row">
-            <div class="col text-center">
-              <h3>CHUCK QUOTES TABLE</h3>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col" id="chucktable" />
-=======
   state = {
     chucks: []
   };
 
   componentDidMount() {
-    fetch("http://ec2-3-19-68-119.us-east-2.compute.amazonaws.com/api/chucks")
+    fetch("https://ec2-3-19-68-119.us-east-2.compute.amazonaws.com/api/chucks")
       .then(res => res.json())
       .then(json => {
         this.setState({ chucks: json });
@@ -94,14 +44,11 @@ class ChuckAppPage extends Component {
                 </tbody>
               ))}
             </table>
->>>>>>> master
           </div>
         </div>
       </div>
     );
   }
-<<<<<<< HEAD
-=======
 
   jsontotable(json) {
     var output = "";
@@ -123,7 +70,6 @@ class ChuckAppPage extends Component {
     }
     return output + "</table>";
   }
->>>>>>> master
 }
 
 export default ChuckAppPage;
